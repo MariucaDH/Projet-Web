09bc2124bd4a69c64e37930de81e669fa8afbf87
Sophie-Anne CORDONNIER <sa.cordonnier@yahoo.fr>
2018-05-04 18:06:24 +0200

    Pasge Accueil avec fil d'actualités

--- accueil.php
+++ accueil.php
@@ -1,4 +1,4 @@
-session_start(); 
+ <?php session_start(); ?>
 <!DOCTYPE html>
     
 <html>
@@ -24,41 +24,102 @@ session_start();
             <div id="bouton"> <a href="emplois.php">Emplois </a> </div>  
             <div id="boutondeco" > <a href="formulaire.php"> Deconnexion </a></div>
         </nav>
-    
-    <div class="actualites">
+       
+       
+
+             
+   <div class="actualites">
         <div id="titre"> BIENVENUE SUR ECExPERTS</div>
         <div id="messageaccueil">Votre fil d'actualité :</div>
-        <div>  <br><br>
-            <div id="nomami">News 1</div> 
-            <img src ="identite.png" class="imgpublication" width = "100px" height = "100px"/> <br><br><br>
-             <div id="nomami">News 2</div> 
-            <img src ="identite.png" class="imgpublication" width = "100px" height = "100px"/> <br><br><br>
-             <div id="nomami">News 3</div> 
-            <img src ="identite.png" class="imgpublication" width = "100px" height = "100px"/> <br><br><br>  
-            <div id="nomami">News 4</div> 
-            <img src ="identite.png" class="imgpublication" width = "100px" height = "100px"/> <br><br><br> 
-            <div id="nomami">News 5</div> 
-            <img src ="identite.png" class="imgpublication" width = "100px" height = "100px"/> <br><br><br> 
+       <br><br><br>
+        <div id="postparpersonne">  
+              <div id="photo"> <img src ="identite.png" class="imgpublication" width = "100px" height = "100px"/><br> photo du user</div>
+                <div id="post">News 1</div> 
+                    <?php
+  
+          try
+                    {
+                        // On se connecte à MySQL
+                        $bdd = new PDO('mysql:host=localhost;dbname=excepert;charset=utf8', 'root','root');
+                    }
+        catch(Exception $e)
+                    {
+                        // En cas d'erreur, on affiche un message et on arrête tout
+                            die('Erreur : '.$e->getMessage());
+                    }
+            //on selectionne le user correspondant a l email
+                    $reponse = $bdd->query('SELECT * FROM post ORDER BY \'date_post\' DESC');
+               
+echo '<div id="post">';
+        while($donnees = $reponse->fetch())
+            //on verifie que le mot de passe correspond 
+    
+        {
+            echo '<br/> Titre : '.$donnees['titre_post'].'<br/>';
+            echo 'Description : '. $donnees['content_post'].'<br/>';  
+            echo 'Date : '.$donnees['date_post'].'<br/>'; 
+            echo 'Humeur : '.$donnees['mood_post'].'<br/>'; 
+          echo '<img src="'.$donnees['file_post'].'" />';
+        }
+               
+    echo '</div>';
+        
+        ?>
+          
+            
          </div>
     </div>
-         
+    
+	
     <div class="publication"> 
         <div id="boutonpartage" > <a> Publier du contenu </a></div>
                 <fieldset>
-	               <p>
-	               <label for="publication">Publication :</label><input/><br/>
-	               <label for="date">Date :</label><input/><br/>
-                   <label for="media">Ajouter un media :</label><br/>
-                   <img src="photo.png" class="logo" height="30" width="30" style="margin-left: auto"/>
-                   <img src="video.png" class="logo" height="30" width="30" style="margin-left: auto"/>  
-                   <img src="event.png" class="logo" height="30" width="30" style="margin-left: auto"/>    
-                       <br/>
-                   <label for="humeur">Feeling :</label><input/><br/>
+	               
+                       <form method="post" action="accueil.php" enctype="multipart/form-data">
+	               <label for="titre">Titre :</label><input name="titre" type="text" id="titre"/><br/>
+                 <label for="descr"> Description : </label><input name="descr" type="text" id="descr"/> <br/>
+                   <label for="media">Ajouter un media :</label><input type="hidden" name="MAX_FILE_SIZE" value="30000"><input type="file" name="file" id="file"/><br/>
+                     
+                   <label for="humeur">Feeling :</label><input name="humeur" type="text" id="humeur"/>
                    <label for="bouton"><a><input type="submit" value="Publier" /></a> </label> 
-	               </p>
+	              </form>
+                           
+        
 	            </fieldset>
          
     </div> 
+        
+        
+        
+        
+        
+        <?php
+       if(empty($_POST['titre'])) $_POST['titre'] = NULL;
+        if(empty($_POST['humeur'])) $_POST['humeur'] = NULL;
+        if(empty($_POST['descr'])) $_POST['descr'] = NULL;
+       if(empty($_POST['file'])) $_POST['file'] = NULL;
+        
+        
+     if(isset($_POST['titre']) AND isset($_POST['descr']))
+     {   //on ouvre la bdd en verifiant son ouverture
+                                try
+                    {
+                                  
+                        // On se connecte à MySQL
+                        $bdd_inscription = new PDO('mysql:host=localhost;dbname=excepert;charset=utf8', 'root','root');
+                    }
+                    catch(Exception $e)
+                    {
+                        // En cas d'erreur, on affiche un message et on arrête tout
+                            die('Erreur : '.$e->getMessage());
+                    }
+            // On ajoute une entrée dans la table user
+            $bdd_inscription->exec('INSERT INTO post (id_user, date_post, content_post, mood_post, file_post, titre_post) VALUES(1,NOW(), \''.$_POST['descr'].'\', \''.$_POST['humeur'].'\',\''.$_POST['file'].'\',\''.$_POST['titre'].'\')');
+           
+            echo 'C bon';
+          
+        }
+        ?>
     </body>
     
         
